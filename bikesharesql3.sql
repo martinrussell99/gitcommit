808be88c6016cfe998df260501a55364e677b051
Martin Russell <russell6606@sbcglobal.net>
2018-07-01 10:09:27 -0500

    bikesaresql3.sql

---
+++ bikesharesql3.sql
@@ -0,0 +1,28 @@
+select
+	trip_id
+from
+	trips
+where
+	duration > 500
+	
+	
+	
+	
+	select
+	*
+from
+	stations
+where
+	station_id = 84
+
+
+
+
+
+select
+	Mintemperaturef
+from
+	weather
+where
+	zip = 94301
+
